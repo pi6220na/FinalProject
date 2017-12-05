5a70f293285ed22ef852350303f2fcd72e2d040e
pi6220na <pi6220na@gmail.com>
2017-12-05 16:08:47 -0600

    Update app.js

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ var ObjectID =  require('mongodb').ObjectID;
 var app = express();
 
 //var mongo_url = process.env.MONGO_URL;
-//var mongo_url = process.env.MONGO_URL;
-var mongo_url = "mongodb://dbUser:456def@ds129946.mlab.com:29946/snake";   //?authSource=admin
+var mongo_url = process.env.MONGO_URL;
+//var mongo_url = "mongodb://dbUser:456def@ds129946.mlab.com:29946/snake";   //?authSource=admin
 
 console.log('app.js    mongo_url = ' + mongo_url);
 
